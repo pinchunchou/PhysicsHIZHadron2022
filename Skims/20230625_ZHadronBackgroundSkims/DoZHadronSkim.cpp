10ac0bacfdf90aa95b53baf111d370f002457f10
pinchun <pinchun@mit.edu>
2024-04-23 20:19:03 -0400

    fix

--- Skims/20230625_ZHadronBackgroundSkims/DoZHadronSkim.cpp
+++ Skims/20230625_ZHadronBackgroundSkims/DoZHadronSkim.cpp
@@ -522,7 +522,7 @@ int main(int argc, char *argv[])
                double TrackResidualCorrection = 1;
                if(DoTrackResidual == true && DoGenCorrelation == false)
                {
-                  TrackResidualCorrection = TrackResidual.GetCorrectionFactor(TrackPT, TrackEta, TrackPhi, /*MZHadron.hiBin + MCHiBinShift*/);
+                  TrackResidualCorrection = TrackResidual.GetCorrectionFactor(TrackPT, TrackEta, TrackPhi, MZHadron.hiBin/* + MCHiBinShift*/);
                }
                MZHadron.trackWeight->push_back(TrackCorrection);
                MZHadron.trackResidualWeight->push_back(TrackResidualCorrection);
