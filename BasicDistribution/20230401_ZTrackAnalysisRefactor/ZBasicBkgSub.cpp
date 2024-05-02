38e8260042c5a721fc228b2f0cd7def2a4e3ffa4
pinchun <pinchun@mit.edu>
2024-05-02 00:23:52 -0400

    BkgSub NoHFVZ_v18e

--- BasicDistribution/20230401_ZTrackAnalysisRefactor/ZBasicBkgSub.cpp
+++ BasicDistribution/20230401_ZTrackAnalysisRefactor/ZBasicBkgSub.cpp
@@ -74,7 +74,7 @@ TFile *file_ppMC;
 TFile *file_ppbkgMC;
 
 
-const char *typeofdata = "v18_PFmuon/20240430/Reco_v18d_NoNcoll_NoHFVZ_v2";
+const char *typeofdata = "v18_PFmuon/20240430/Reco_v18e_NoNcoll_NoHFVZ";
 //const char *typeofdata = "testBkgSub/20240204/v17d_No1Sub";
 //const char *typeofdata1 = "no1sub";
 //const char *typeofdata1 = "37_ov10_RECO_PP";
@@ -522,9 +522,9 @@ int main(int argc, char *argv[]){
       file_bkgMC = TFile::Open("~/eos_base/BasicPlots/GraphMCSigBkg_v17d_PFmuon_350_10HF_ov20.root","read");
    else{
       if(isgen)
-         file_bkgMC = TFile::Open("~/eos_base/BasicPlots/GraphMCBackground_v18d_NoHFVZ_NoNcoll_v2.root","read");
+         file_bkgMC = TFile::Open("~/eos_base/BasicPlots/GraphMCBackground_v18e_NoHFVZ_NoNcoll.root","read");
       else
-         file_bkgMC = TFile::Open("~/eos_base/BasicPlots/GraphMCBackground_v18d_NoHFVZ_NoNcoll_v2.root","read");
+         file_bkgMC = TFile::Open("~/eos_base/BasicPlots/GraphMCBackground_v18e_NoHFVZ_NoNcoll.root","read");
    }
 
    if(isgen)
