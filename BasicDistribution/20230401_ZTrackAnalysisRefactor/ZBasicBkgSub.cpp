a766a77565e5a5fe6a204152f5546d47fbff062e
pinchun <pinchun@mit.edu>
2024-05-14 16:27:51 -0400

    BkgSub v18e_770_413_ov1_noW

--- BasicDistribution/20230401_ZTrackAnalysisRefactor/ZBasicBkgSub.cpp
+++ BasicDistribution/20230401_ZTrackAnalysisRefactor/ZBasicBkgSub.cpp
@@ -74,7 +74,7 @@ TFile *file_ppMC;
 TFile *file_ppbkgMC;
 
 
-const char *typeofdata = "v18_PFmuon/20240514/Reco_v18e_663_671_ov1_noW";
+const char *typeofdata = "v18_PFmuon/20240514/Reco_v18e_770_413_ov1_noW";
 //const char *typeofdata = "testBkgSub/20240204/v17d_No1Sub";
 //const char *typeofdata1 = "no1sub";
 //const char *typeofdata1 = "37_ov10_RECO_PP";
@@ -535,7 +535,7 @@ int main(int argc, char *argv[]){
       if(isgen)
          file_bkgMC = TFile::Open("~/eos_base/BasicPlots/GraphMCBackground_v18e_880_671_ov1.root","read");
       else
-         file_bkgMC = TFile::Open("~/eos_base/BasicPlots/GraphMCBackground_v18e_ov1_noW.root","read");
+         file_bkgMC = TFile::Open("~/eos_base/BasicPlots/GraphMCBackground_v18e_770_413_ov1_hibin3_noW.root","read");
    }
 
    if(isgen)
